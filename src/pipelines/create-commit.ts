b352ff2ca8b64f2f3e172f6c0ebc28058c928e9c
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-04-20 15:12:29 +0800

    feat(commit): implement createCommitPipeline with staging functionality

--- src/pipelines/create-commit.ts
+++ src/pipelines/create-commit.ts
@@ -1,6 +1,24 @@
-import { okAsync, type ResultAsync } from "neverthrow";
+import { colors } from "consola/utils";
+import { type ResultAsync } from "neverthrow";
+import { resolveCommitMessage } from "#/lib/config";
+import { executeGit } from "#/lib/git";
+import { logger } from "#/lib/logger";
 import type { ArtemisContext } from "#/types";
 
 export function createCommitPipeline(context: ArtemisContext): ResultAsync<ArtemisContext, Error> {
-    return okAsync(context);
+    return stageFiles(context).andThen(createCommit);
+}
+
+function createCommit(context: ArtemisContext): ResultAsync<ArtemisContext, Error> {
+    const commitMessage: string = resolveCommitMessage(context);
+
+    return executeGit(["commit", "-m", commitMessage])
+        .andTee((): void => logger.info(`Created commit with message: ${colors.dim(commitMessage)}`))
+        .map((): ArtemisContext => context);
+}
+
+function stageFiles(context: ArtemisContext) {
+    return executeGit(["add", "."])
+        .andTee((): void => logger.info("Staged all files for commit"))
+        .map((): ArtemisContext => context);
 }
