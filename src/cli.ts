36e3c4d1374e592f9aca56e23e5a1f743380c168
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-05-07 08:35:58 +0800

    feat: implement CLI command structure and validation functions

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,25 @@
 #!/usr/bin/env bun
 
-console.log("Hello, world!");
+import { colors } from "consola/utils";
+import { okAsync, type ResultAsync } from "neverthrow";
+import { cli } from "#/infrastructure/commander";
+import type { ArtemisOptions } from "#/infrastructure/config";
+import { logger } from "#/infrastructure/logging";
+import pkg from "../package.json" with { type: "json" };
+
+export async function main(): Promise<void> {
+    cli.action((options: ArtemisOptions): Promise<void> => {
+        logger.log(`${colors.magenta("artemis")} ${colors.dim(`v${pkg.version}`)}`);
+        return new Promise<void>((): ResultAsync<void, Error> => okAsync(undefined));
+    });
+
+    cli.configureOutput({
+        writeErr(str: string): void {
+            logger.error(str);
+        }
+    });
+
+    cli.parse(Bun.argv);
+}
+
+await main();
