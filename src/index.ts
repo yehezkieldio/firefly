7ac375eaf6ad4b5da9db3e65e064732398e49719
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-05-07 08:24:04 +0800

    feat: define configuration function for Artemis release process

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import type { ArtemisOptions } from "#/core/config";
+
+/**
+ * Defines the configuration for the Artemis release process.
+ */
+export function defineConfig(options: ArtemisOptions): ArtemisOptions {
+    return options;
+}
+
+export type { ArtemisOptions };
