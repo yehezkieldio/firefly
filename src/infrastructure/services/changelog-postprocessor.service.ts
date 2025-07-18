f446726624d38e974c79d43c56ab993fb529427a
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-07-18 23:18:08 +0800

    refactor: remove versioning and changelog related components

--- src/infrastructure/services/changelog-postprocessor.service.ts
+++
@@ -1,9 +0,0 @@
-import type { GitCliffConfigService } from "#/infrastructure/services/git-cliff-config.service";
-
-export class ChangelogPostProcessor {
-    constructor(private cliffService: GitCliffConfigService) {}
-
-    async process(rawChangelog: string): Promise<string> {
-        return await this.cliffService.removeHeaderFromChangelog(rawChangelog);
-    }
-}
--- src/shared/logger.ts
+++
@@ -1,8 +0,0 @@
-import { type ConsolaInstance, createConsola } from "consola";
-
-export const logger: ConsolaInstance = createConsola({
-    formatOptions: {
-        date: false,
-        compact: true,
-    },
-});
