d7fa6119b5a254606ef8b04f2d03105bc62d0f49
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-07-19 20:29:18 +0800

    refactor: remove deprecated versioning and CLI services

--- src/shared/constants/conventional.ts
+++
@@ -1,8 +0,0 @@
-export const CONVENTIONAL_OPTIONS = {
-    headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
-    headerCorrespondence: ["type", "scope", "subject"],
-    noteKeywords: ["BREAKING CHANGE"],
-    revertPattern: /^(?:Revert|revert:)\s"?([\s\S]+?)"?\s*This reverts commit (\w*)\./i,
-    revertCorrespondence: ["header", "hash"],
-    breakingHeaderPattern: /^(\w*)(?:\((.*)\))?!: (.*)$/,
-};
--- src/shared/utils/safe-json-parse.ts
+++
@@ -1,6 +0,0 @@
-import { Result } from "neverthrow";
-import { ParseError } from "#/shared/result";
-
-const toParseError = (e: unknown) => new ParseError("Failed to parse JSON", e as Error);
-
-export const safeJsonParse = Result.fromThrowable(JSON.parse, toParseError);
