9a0437cbc40f6fcf95c222da270e7ec325dda0db
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-07-27 20:51:14 +0800

    style(result): enhance documentation for FireflyResult and AsyncFireflyResult types

--- src/shared/utils/result.util.ts
+++ src/shared/utils/result.util.ts
@@ -1,5 +1,32 @@
 import type { Result, ResultAsync } from "neverthrow";
 import type { FireflyError } from "#/shared/utils/error.util";
 
+/**
+ * Represents a result that can be returned from a function.
+ *
+ * @example
+ * ```ts
+ * const result = someFunction(); // someFunction returns FireflyResult<T>
+ * if (result.isErr()) {
+ *  // Handle the error
+ * }
+ *
+ * result.value; // This will be the successful value of type T
+ * ```
+ */
 export type FireflyResult<T> = Result<T, FireflyError>;
+
+/**
+ * Represents a result that can be returned that needs to be awaited.
+ *
+ * @example
+ * ```ts
+ * const result = await someAsyncFunction(); // someAsyncFunction returns AsyncFireflyResult<T>
+ * if (result.isErr()) {
+ *  // Handle the error
+ * }
+ *
+ * result.value; // This will be the successful value of type T
+ * ```
+ */
 export type AsyncFireflyResult<T> = ResultAsync<T, FireflyError>;
