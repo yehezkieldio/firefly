9ebe98d0fef3a9423e3233802a2fb9e83f6216f6
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-08-17 01:35:32 +0800

    feat(config): add helper function to define file configuration

---
+++ src/platform/config/index.ts
@@ -0,0 +1,10 @@
+import type { __FireflyConfig } from "#/modules/configuration/application/schema.registry";
+
+export type FireflyConfig = Partial<__FireflyConfig>;
+
+/**
+ * Optional helper function to define Firefly configuration.
+ */
+export function defineConfig<T extends FireflyConfig>(options: T): T {
+    return options;
+}
