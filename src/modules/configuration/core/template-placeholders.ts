0e11133ca3e006c63eb80c2eb84c62977827f3bc
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-08-17 19:09:20 +0800

    chore: clean codebase (again)
    Seriously, my perfectionism is extreme at this point. Send Help.

--- src/modules/configuration/core/template-placeholders.ts
+++
@@ -1,3 +0,0 @@
-export const VALID_TEMPLATE_PLACEHOLDERS = ["{{name}}", "{{unscopedName}}", "{{version}}"] as const;
-
-export type TemplatePlaceholder = (typeof VALID_TEMPLATE_PLACEHOLDERS)[number];
--- src/modules/semver/core/constants/pre-release-base.constant.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-/**
- * Defines the base version for a pre-release.
- * Accepts either a number or the string "0" or "1", representing the starting point of the pre-release cycle.
- * This field is optional to allow flexibility in pre-release versioning.
- */
-export const PreReleaseBaseSchema = z.union([z.number(), z.literal("0"), z.literal("1")]).optional();
-export type PreReleaseBaseType = z.infer<typeof PreReleaseBaseSchema>;
