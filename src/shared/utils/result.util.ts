54b43c512722b903b7b34e533f7322df082671f8
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-09-05 07:28:36 +0800

    refactor(utils): remove redundant template annotations

--- src/shared/utils/result.util.ts
+++ src/shared/utils/result.util.ts
@@ -5,8 +5,6 @@ import { type FireflyError, createFireflyError, toFireflyError } from "#/shared/
 /**
  * - Represents `Result<T, FireflyError>`.
  * - Can be wrapped in a `Promise<FireflyResult<T>>` when internally unwrapping a `FireflyAsyncResult<T>`.
- *
- * @template T The success value type.
  */
 export type FireflyResult<T> = Result<T, FireflyError>;
 
@@ -14,8 +12,6 @@ export type FireflyResult<T> = Result<T, FireflyError>;
  * - Represents `ResultAsync<T, FireflyError>`.
  * - **Do not** wrap in another `Promise`.
  * - **Do not** mark functions returning this type as `async`.
- *
- * @template T The success value type.
  */
 export type FireflyAsyncResult<T> = ResultAsync<T, FireflyError>;
 
