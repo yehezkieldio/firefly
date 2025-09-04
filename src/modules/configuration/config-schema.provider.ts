13141a1cc3008d91b0b3c7fbc1b45100f07bf4b4
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-09-05 06:13:57 +0800

    feat(configuration): add config schema provider

---
+++ src/modules/configuration/config-schema.provider.ts
@@ -0,0 +1,42 @@
+import type { z } from "zod";
+import { BaseConfigSchema } from "#/modules/configuration/schema/config-base.schema";
+import { ReleaseConfigSchema } from "#/modules/configuration/schema/config-release.schema";
+
+const schemas = {
+    release: ReleaseConfigSchema,
+} as const;
+
+export class ConfigSchemaProvider {
+    private static readonly schemas = schemas;
+
+    static base() {
+        return BaseConfigSchema;
+    }
+
+    static get(command?: CommandName) {
+        if (command) {
+            const commandSchema = ConfigSchemaProvider.schemas[command];
+            return BaseConfigSchema.extend({
+                ...commandSchema.shape,
+            });
+        }
+
+        const mergedShape: z.ZodRawShape = {};
+        for (const schema of Object.values(ConfigSchemaProvider.schemas)) {
+            Object.assign(mergedShape, schema.shape);
+        }
+
+        return BaseConfigSchema.extend(mergedShape);
+    }
+}
+
+export type BaseConfig = z.infer<typeof BaseConfigSchema>;
+
+export type CommandName = keyof typeof schemas;
+export type CommandConfigMap = {
+    [K in keyof typeof schemas]: z.infer<(typeof schemas)[K]>;
+};
+
+export type FinalConfigFor<C extends CommandName> = BaseConfig & CommandConfigMap[C];
+
+export type _FireflyConfig = FinalConfigFor<CommandName>;
