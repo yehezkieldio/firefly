7b21b175f7105292fefeee182e2be2f5ad2d9fef
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-09-12 07:00:23 +0800

    feat(github): add release service to GitHub provider

--- src/modules/github/github.provider.ts
+++ src/modules/github/github.provider.ts
@@ -1,9 +1,11 @@
+import { GitHubReleaseService } from "#/modules/github/services/gh-release.service";
 import { GitHubTokenService } from "#/modules/github/services/gh-token.service";
 
 export class GitHubProvider {
     private static _instance: GitHubProvider;
 
     private _token?: GitHubTokenService;
+    private _release?: GitHubReleaseService;
 
     private constructor() {}
 
@@ -19,5 +21,12 @@ export class GitHubProvider {
             this._token = new GitHubTokenService();
         }
         return this._token;
+    }
+
+    get release(): GitHubReleaseService {
+        if (!this._release) {
+            this._release = new GitHubReleaseService();
+        }
+        return this._release;
     }
 }
