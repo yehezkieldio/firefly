58ebdea23bdb0c7474ce09afca082fa709a30e64
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-09-12 07:46:22 +0800

    feat(git): implement tag pushing to remote repository

--- src/modules/git/tasks/push-tag.task.ts
+++ src/modules/git/tasks/push-tag.task.ts
@@ -1,9 +1,12 @@
-import { ok, okAsync } from "neverthrow";
+import { errAsync, ok } from "neverthrow";
 import type { ReleaseTaskContext } from "#/application/context";
+import { ReleaseTemplateResolverService } from "#/modules/configuration/services/release-template-resolver.service";
+import { GitProvider } from "#/modules/git/git.provider";
 import { PushCommitTask } from "#/modules/git/tasks/push-commit.task";
 import type { ConditionalTask } from "#/modules/orchestration/contracts/task.interface";
 import { taskRef } from "#/modules/orchestration/utils/task-ref.util";
-import type { FireflyAsyncResult, FireflyResult } from "#/shared/utils/result.util";
+import { logger } from "#/shared/logger";
+import { type FireflyAsyncResult, type FireflyResult, wrapPromise } from "#/shared/utils/result.util";
 
 export class PushTagTask implements ConditionalTask<ReleaseTaskContext> {
     readonly id = "push-tag";
@@ -23,7 +26,29 @@ export class PushTagTask implements ConditionalTask<ReleaseTaskContext> {
         return ok([]);
     }
 
-    execute(_context: ReleaseTaskContext): FireflyAsyncResult<void> {
-        return okAsync();
+    execute(context: ReleaseTaskContext): FireflyAsyncResult<void> {
+        const gitProvider = GitProvider.getInstance();
+        const config = context.getConfig();
+        const releaseTemplateResolverService = new ReleaseTemplateResolverService().withContext({
+            version: context.getNextVersion(),
+            config: context.getConfig(),
+        });
+        const tagName = releaseTemplateResolverService.tagName(context.getConfig().tagName);
+        const remoteResultAsync = wrapPromise(gitProvider.remote.getCurrentRemote());
+
+        return remoteResultAsync
+            .andThen((remoteName) => {
+                if (remoteName.isErr()) {
+                    return errAsync(remoteName.error);
+                }
+
+                return wrapPromise(gitProvider.push.pushTag(tagName, remoteName.value, config.dryRun));
+            })
+            .orElse(() => {
+                return wrapPromise(gitProvider.push.pushTag(tagName, "origin", config.dryRun));
+            })
+            .map(() => {
+                logger.success("Pushed tag to remote repository");
+            });
     }
 }
