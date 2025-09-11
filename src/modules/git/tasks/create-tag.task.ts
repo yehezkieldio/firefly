f69943f1bd68d55ec3f6f940827250c07db61dea
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-09-12 07:13:43 +0800

    feat(git): implement create and delete tag functionality

--- src/modules/git/tasks/create-tag.task.ts
+++ src/modules/git/tasks/create-tag.task.ts
@@ -1,10 +1,14 @@
-import { ok, okAsync } from "neverthrow";
+import { colors } from "consola/utils";
+import { ok } from "neverthrow";
 import type { ReleaseTaskContext } from "#/application/context";
+import { ReleaseTemplateResolverService } from "#/modules/configuration/services/release-template-resolver.service";
+import { GitProvider } from "#/modules/git/git.provider";
 import { CommitChangesTask } from "#/modules/git/tasks/commit-changes.task";
 import { PushCommitTask } from "#/modules/git/tasks/push-commit.task";
 import type { ConditionalTask } from "#/modules/orchestration/contracts/task.interface";
 import { taskRef } from "#/modules/orchestration/utils/task-ref.util";
-import type { FireflyAsyncResult, FireflyResult } from "#/shared/utils/result.util";
+import { logger } from "#/shared/logger";
+import { type FireflyAsyncResult, type FireflyResult, wrapPromise } from "#/shared/utils/result.util";
 
 export class CreateTagTask implements ConditionalTask<ReleaseTaskContext> {
     readonly id = "create-tag";
@@ -29,7 +33,34 @@ export class CreateTagTask implements ConditionalTask<ReleaseTaskContext> {
         return ok([taskRef(PushCommitTask)]);
     }
 
-    execute(_context: ReleaseTaskContext): FireflyAsyncResult<void> {
-        return okAsync();
+    execute(context: ReleaseTaskContext): FireflyAsyncResult<void> {
+        const gitProvider = GitProvider.getInstance();
+        const releaseTemplateResolverService = new ReleaseTemplateResolverService().withContext({
+            version: context.getNextVersion(),
+            config: context.getConfig(),
+        });
+        const tagName = releaseTemplateResolverService.tagName(context.getConfig().tagName);
+
+        return wrapPromise(gitProvider.tag.createTag(tagName, undefined, context.getConfig().dryRun)).map(() => {
+            logger.info(`Created tag: ${colors.gray(tagName)}`);
+        });
+    }
+
+    canUndo(): boolean {
+        return true;
+    }
+
+    undo(context: ReleaseTaskContext): FireflyAsyncResult<void> {
+        const gitProvider = GitProvider.getInstance();
+        const releaseTemplateResolverService = new ReleaseTemplateResolverService().withContext({
+            version: context.getNextVersion(),
+            config: context.getConfig(),
+        });
+
+        const tagName = releaseTemplateResolverService.tagName(context.getConfig().tagName);
+
+        return wrapPromise(gitProvider.tag.deleteLocal(tagName, context.getConfig().dryRun)).map(() => {
+            logger.info(`Deleted tag: ${colors.gray(tagName)}`);
+        });
     }
 }
