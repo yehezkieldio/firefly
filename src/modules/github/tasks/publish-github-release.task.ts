77b6dae3f26bb3875391d6ec720ff8a7cffa2c1b
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-09-12 08:13:33 +0800

    feat(github): implement GitHub release publishing logic

--- src/modules/github/tasks/publish-github-release.task.ts
+++ src/modules/github/tasks/publish-github-release.task.ts
@@ -1,9 +1,15 @@
-import { ok, okAsync } from "neverthrow";
+import { colors } from "consola/utils";
+import { errAsync, ok } from "neverthrow";
 import type { ReleaseTaskContext } from "#/application/context";
+import { ChangelogPostProcessorService } from "#/modules/changelog/services/changelog-postprocessor.service";
+import { ReleaseTemplateResolverService } from "#/modules/configuration/services/release-template-resolver.service";
+import { CliffTomlService } from "#/modules/filesystem/cliff-toml.service";
+import { GitHubProvider } from "#/modules/github/github.provider";
 import type { ConditionalTask } from "#/modules/orchestration/contracts/task.interface";
 import { PlatformPublishControllerTask } from "#/modules/orchestration/tasks";
 import { taskRef } from "#/modules/orchestration/utils/task-ref.util";
-import type { FireflyAsyncResult, FireflyResult } from "#/shared/utils/result.util";
+import { logger } from "#/shared/logger";
+import { type FireflyAsyncResult, type FireflyResult, wrapPromise } from "#/shared/utils/result.util";
 
 export class PublishGitHubReleaseTask implements ConditionalTask<ReleaseTaskContext> {
     readonly id = "publish-github-release";
@@ -21,7 +27,55 @@ export class PublishGitHubReleaseTask implements ConditionalTask<ReleaseTaskCont
         return ok(true);
     }
 
-    execute(_context: ReleaseTaskContext): FireflyAsyncResult<void> {
-        return okAsync();
+    execute(context: ReleaseTaskContext): FireflyAsyncResult<void> {
+        const config = context.getConfig();
+        const changelogContent = context.get("changelogContent");
+        if (changelogContent.isErr()) {
+            return errAsync(changelogContent.error);
+        }
+
+        const ghProvider = GitHubProvider.getInstance();
+        const cliffTomlParse = CliffTomlService.getInstance(context.getBasePath());
+        const releaseTemplateResolverService = new ReleaseTemplateResolverService().withContext({
+            version: context.getNextVersion(),
+            config: context.getConfig(),
+        });
+        const releaseTitle = releaseTemplateResolverService.releaseTitle(context.getConfig().releaseTitle);
+        const tagName = releaseTemplateResolverService.tagName(context.getConfig().tagName);
+        const changelogPostProcessor = new ChangelogPostProcessorService(cliffTomlParse);
+        const processChangelog = wrapPromise(changelogPostProcessor.process(changelogContent.value as string));
+
+        const releaseStatus =
+            [
+                config.releaseDraft ? "draft" : null,
+                config.releasePreRelease ? "pre-release" : null,
+                config.releaseLatest ? "latest" : null,
+            ]
+                .filter((status) => status !== null)
+                .join(", ") || "normal";
+
+        logger.info(`Publishing a ${colors.gray(releaseStatus)} GitHub release.`);
+
+        return processChangelog
+            .andThen((changelog) => {
+                if (changelog.isErr()) {
+                    return errAsync(changelog.error);
+                }
+
+                const release = ghProvider.release.createRelease({
+                    title: releaseTitle,
+                    tag: tagName,
+                    content: changelog.value,
+                    latest: config.releaseLatest,
+                    draft: config.releaseDraft,
+                    prerelease: config.releasePreRelease,
+                    dryRun: config.dryRun,
+                });
+
+                return wrapPromise(release);
+            })
+            .map(() => {
+                logger.info("GitHub release published successfully.");
+            });
     }
 }
