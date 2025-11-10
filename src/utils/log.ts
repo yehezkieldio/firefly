2b78efdbdb20162a56133ec8a439a95b5bdf66e6
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-11-11 06:59:42 +0800

    feat(utils): add error handling and logging modules

---
+++ src/utils/log.ts
@@ -0,0 +1,27 @@
+import { type ConsolaInstance, createConsola, type FormatOptions, type LogObject } from "consola";
+import { colors } from "consola/utils";
+
+const opts: FormatOptions = {
+    date: false,
+    compact: true,
+    columns: 0,
+};
+
+const _logger = createConsola({
+    formatOptions: opts,
+});
+
+export const logger: ConsolaInstance = createConsola({
+    formatOptions: opts,
+    reporters: [
+        {
+            log(logObj: LogObject) {
+                if (logObj.type === "verbose") {
+                    console.log(colors.gray(logObj.args.join(" ")));
+                } else {
+                    _logger.log(logObj);
+                }
+            },
+        },
+    ],
+});
