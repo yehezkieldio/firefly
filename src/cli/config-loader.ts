5868190bf8e1743e3f08d487f421675996361e70
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-11-29 10:27:40 +0800

    refactor(config-loader): replace ResultAsync.fromPromise with wrapPromise

--- src/cli/config-loader.ts
+++ src/cli/config-loader.ts
@@ -1,11 +1,11 @@
 import { type ConfigLayerMeta, loadConfig, type ResolvedConfig } from "c12";
 import { colors } from "consola/utils";
-import { errAsync, okAsync, ResultAsync } from "neverthrow";
+import { errAsync, okAsync } from "neverthrow";
 import type { z } from "zod";
 import type { CommandConfig } from "#/cli/types";
 import { createFireflyError, toFireflyError } from "#/utils/error";
 import { logger } from "#/utils/log";
-import type { FireflyAsyncResult } from "#/utils/result";
+import { type FireflyAsyncResult, wrapPromise } from "#/utils/result";
 
 export interface ConfigLoaderOptions {
     cwd?: string;
@@ -20,14 +20,13 @@ export class ConfigLoader {
     load(): FireflyAsyncResult<CommandConfig> {
         const { cwd = process.cwd(), configFile, schema } = this.options;
 
-        return ResultAsync.fromPromise(
+        return wrapPromise(
             loadConfig<CommandConfig>({
                 name: "firefly",
                 cwd,
                 configFile: configFile || "firefly.config",
                 packageJson: false,
-            }),
-            (error: unknown) => createFireflyError(toFireflyError(error))
+            })
         ).andThen((result: ResolvedConfig<CommandConfig, ConfigLayerMeta>) => {
             this.logConfigFile(result.configFile);
             const finalConfig = this.extractCommandConfig(result.config ?? {});
